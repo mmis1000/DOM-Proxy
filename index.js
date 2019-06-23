42dd98f137e74c72507989a14e480be244a75055
mmis1000 <mmis10002@gmail.com>
2019-06-23 08:05:44 +0000

    refactor

--- index.js
+++ index.js
@@ -1,39 +1,38 @@
-/**
- * @type {ArrayBuffer}
- */
-const buffer = new SharedArrayBuffer(1024 * 1024)
-const int32 = new Int32Array(buffer);
-const dataView = new DataView(buffer)
+async function main (handler) {
+    /**
+     * @type {ArrayBuffer}
+     */
+    const buffer = new SharedArrayBuffer(1024 * 1024)
+    const int32 = new Int32Array(buffer);
+    const dataView = new DataView(buffer)
 
-const myWorker = new Worker("worker.js");
-let id = 0
+    const myWorker = new Worker("worker.js");
+    let id = 0
 
-const I32_PARENT_LOCK_INDEX = 0
-const I32_CHILD_LOCK_INDEX = 1
-const I32_DATA_LENGTH_INDEX = 2
-const I32_DATA_INDEX = 3
-const DATA_LRNGTH_LIMIT = 1024 * 1024 - 8
+    const I32_PARENT_LOCK_INDEX = 0
+    const I32_CHILD_LOCK_INDEX = 1
+    const I32_DATA_LENGTH_INDEX = 2
+    const I32_DATA_INDEX = 3
+    const DATA_LRNGTH_LIMIT = 1024 * 1024 - 8
 
-const decoder = new TextDecoder()
-const encoder = new TextEncoder()
+    const decoder = new TextDecoder()
+    const encoder = new TextEncoder()
 
-/**
- * @param {ArrayBuffer} buffer
- */
-function parse(buffer, offset, length) {
-    var slice = buffer.slice(offset, offset + length)
-    var text = decoder.decode(new Uint8Array(new Uint8Array(slice)))
-    return text
-}
+    /**
+     * @param {ArrayBuffer} buffer
+     */
+    function parse(buffer, offset, length) {
+        var slice = buffer.slice(offset, offset + length)
+        var text = decoder.decode(new Uint8Array(new Uint8Array(slice)))
+        return text
+    }
 
-function write(buffer, offset, text) {
-    var encodedBuffer = encoder.encode(text)
-    new Uint8Array(buffer).set(encodedBuffer, offset)
+    function write(buffer, offset, text) {
+        var encodedBuffer = encoder.encode(text)
+        new Uint8Array(buffer).set(encodedBuffer, offset)
 
-    return encodedBuffer.byteLength
-}
-
-async function main () {
+        return encodedBuffer.byteLength
+    }
 
     myWorker.postMessage({
         constants: {
@@ -49,12 +48,6 @@ async function main () {
     })
 
     let old = 0;
-
-    async function handler (request) {
-        console.log('main: received requset ', text, Date.now())
-        // await new Promise(resolve => setTimeout(resolve, 10))
-        return '[Callback] ' + request
-    }
 
     while (true) {
 
@@ -73,4 +66,8 @@ async function main () {
     
 }
 
-main()
+main(async function handler (request) {
+    console.log('main: received requset ', request, Date.now())
+    // await new Promise(resolve => setTimeout(resolve, 10))
+    return '[Callback] ' + request
+})
--- worker.js
+++ worker.js
@@ -1,43 +1,49 @@
-self.addEventListener('message', function (event) {
-    console.log('recieve', event)
-    var data = event.data
-    var buffer = data.buffer
-    var int32 = data.int32
-    var constants = data.constants
-    var dataView = data.dataView
-    let id = 0
-
-    const decoder = new TextDecoder()
-    const encoder = new TextEncoder()
+function main (callback) {
+    self.addEventListener('message', function (event) {
+        console.log('recieve', event)
+        var data = event.data
+        var buffer = data.buffer
+        var int32 = data.int32
+        var constants = data.constants
+        var dataView = data.dataView
+        let id = 0
     
-    function parse(buffer, offset, length) {
-        var slice = buffer.slice(offset, offset + length)
-        var text = decoder.decode(new Uint8Array(new Uint8Array(slice)))
-        return text
-    }
+        const decoder = new TextDecoder()
+        const encoder = new TextEncoder()
+        
+        function parse(buffer, offset, length) {
+            var slice = buffer.slice(offset, offset + length)
+            var text = decoder.decode(new Uint8Array(new Uint8Array(slice)))
+            return text
+        }
+    
+        function write(buffer, offset, text) {
+            var encodedBuffer = encoder.encode(text)
+            new Uint8Array(buffer).set(encodedBuffer, offset)
+    
+            return encodedBuffer.byteLength
+        }
+    
+        var send = (text) => {
+            var length = write(buffer, constants.I32_DATA_INDEX * 4, text)
+            dataView.setUint32(constants.I32_DATA_LENGTH_INDEX * 4, length)
+    
+            const old = Atomics.load(int32, constants.I32_CHILD_LOCK_INDEX)
+            Atomics.store(int32, constants.I32_PARENT_LOCK_INDEX, id++)
+            Atomics.notify(int32, constants.I32_PARENT_LOCK_INDEX)
+            Atomics.wait(int32, constants.I32_CHILD_LOCK_INDEX, old)
+    
+            var length = dataView.getUint32(constants.I32_DATA_LENGTH_INDEX * 4)
+            var text = parse(buffer, constants.I32_DATA_INDEX * 4, length)
+    
+            return text
+        }
+    
+        callback(send)
+    })
+}
 
-    function write(buffer, offset, text) {
-        var encodedBuffer = encoder.encode(text)
-        new Uint8Array(buffer).set(encodedBuffer, offset)
-
-        return encodedBuffer.byteLength
-    }
-
-    var send = (text) => {
-        var length = write(buffer, constants.I32_DATA_INDEX * 4, text)
-        dataView.setUint32(constants.I32_DATA_LENGTH_INDEX * 4, length)
-
-        const old = Atomics.load(int32, constants.I32_CHILD_LOCK_INDEX)
-        Atomics.store(int32, constants.I32_PARENT_LOCK_INDEX, id++)
-        Atomics.notify(int32, constants.I32_PARENT_LOCK_INDEX)
-        Atomics.wait(int32, constants.I32_CHILD_LOCK_INDEX, old)
-
-        var length = dataView.getUint32(constants.I32_DATA_LENGTH_INDEX * 4)
-        var text = parse(buffer, constants.I32_DATA_INDEX * 4, length)
-
-        return text
-    }
-
+main(function (send) {
     const start = Date.now()
 
     for (var i = 0; i < 1000; i++) {
