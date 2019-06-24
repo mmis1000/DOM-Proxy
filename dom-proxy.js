32ad4baa81f0ce4ac09490a6299b549694cf784e
mmis1000 <mmis10002@gmail.com>
2019-06-24 06:06:36 +0000

    Wrap dom proxy into single file and clean up

---
+++ dom-proxy.js
@@ -0,0 +1,257 @@
+var global = typeof window !== 'undefined' ? window : self;
+
+{
+    const I32_PARENT_LOCK_INDEX = 0
+    const I32_CHILD_LOCK_INDEX = 1
+    const I32_DATA_LENGTH_INDEX = 2
+    const I32_DATA_INDEX = 3
+    const DATA_LRNGTH_LIMIT = 1024 * 1024 - 8
+
+            
+    const COMMANDS = {
+        GET_ROOT: 'GET_ROOT',
+        GET_PROPERTY: 'GET_PROPERTY',
+        GET_OWN_KEYS: 'GET_OWN_KEYS',
+        GET_OWN_PROPERTY_DESCRIPTOR: 'GET_OWN_PROPERTY_DESCRIPTOR',
+        UNREF: 'UNREF'
+    }
+
+    const TYPES = {
+        NUMBER: 'NUMBER',
+        BOOLEAN: 'NUMBER',
+        STRING: 'STRING',
+        NULL: 'NULL',
+        UNDEFINED: 'UNDEFINED',
+        OBJECT: 'OBJECT'
+    }
+
+    var DOMProxy = global.DOMProxy = {
+        constants: {
+            OFFSETS: {
+                I32_PARENT_LOCK_INDEX,
+                I32_CHILD_LOCK_INDEX,
+                I32_DATA_LENGTH_INDEX,
+                I32_DATA_INDEX
+            },
+            LIMITS: {
+                DATA_LRNGTH_LIMIT
+            },
+            COMMANDS,
+            TYPES
+        },
+        createHOST () {
+            /**
+             * @type {ArrayBuffer}
+             */
+            const buffer = new SharedArrayBuffer(1024 * 1024)
+            const int32 = new Int32Array(buffer);
+            const dataView = new DataView(buffer)
+
+            let id = 0
+        
+        
+            const decoder = new TextDecoder()
+            const encoder = new TextEncoder()
+            
+            /**
+             * @param {ArrayBuffer} buffer
+             */
+            function parse(buffer, offset, length) {
+                var slice = buffer.slice(offset, offset + length)
+                var text = decoder.decode(new Uint8Array(new Uint8Array(slice)))
+                return text
+            }
+        
+            function write(buffer, offset, text) {
+                var encodedBuffer = encoder.encode(text)
+                new Uint8Array(buffer).set(encodedBuffer, offset)
+        
+                return encodedBuffer.byteLength
+            }
+
+            const payload = {
+                constants: {
+                    I32_PARENT_LOCK_INDEX,
+                    I32_CHILD_LOCK_INDEX,
+                    I32_DATA_LENGTH_INDEX,
+                    I32_DATA_INDEX,
+                    DATA_LRNGTH_LIMIT
+                },
+                buffer,
+                int32,
+                dataView
+            }
+
+            async function listen (handler) {
+                let old = 0;
+            
+                while (true) {
+            
+                    await Atomics.waitAsync(int32, I32_PARENT_LOCK_INDEX, old)
+            
+                    var length = dataView.getUint32(I32_DATA_LENGTH_INDEX * 4)
+                    var text = parse(buffer, I32_DATA_INDEX * 4, length)
+            
+                    var writeLength = write(buffer, I32_DATA_INDEX * 4, await handler(text))
+                    dataView.setUint32(I32_DATA_LENGTH_INDEX * 4, writeLength)
+            
+                    old = Atomics.load(int32, I32_PARENT_LOCK_INDEX)
+                    Atomics.store(int32, I32_CHILD_LOCK_INDEX, id++)
+                    Atomics.notify(int32, I32_CHILD_LOCK_INDEX)
+                }
+                
+            }
+            
+            let refId = 0
+            const map = new Map()
+            const backMap = new WeakMap()
+            
+            function format (item) {
+                if (item === null) {
+                    return { type: TYPES.NULL }
+                } else if (item === undefined) {
+                    return { type: TYPES.UNDEFINED }
+                } else {
+                    switch (typeof item) {
+                        case "boolean":
+                            return { type: TYPES.BOOLEAN, value: item }
+                        case "number": 
+                            return { type: TYPES.NUMBER, value: item }
+                        case "string": 
+                            return { type: TYPES.STRING, value: item }
+                        case "function":
+                        case "object":
+                            var id
+                            if (!backMap.has(item)) {
+                                id = refId++
+                                map.set(id, item)
+                                backMap.set(item, id)
+                            } else {
+                                id = backMap.get(item)
+                            }
+            
+                            return { type: TYPES.OBJECT, ref: id }
+                    }
+                }
+            }
+            
+            listen(function handler (requestText) {
+                var request = JSON.parse(requestText)
+            
+                switch (request.command) {
+                    case COMMANDS.GET_ROOT:
+                        return JSON.stringify(format(window))
+                    case COMMANDS.GET_PROPERTY:
+                        var self = map.get(request.self)
+                        var prop = request.prop
+                        return JSON.stringify(format(self[prop]))
+                    case COMMANDS.GET_OWN_KEYS:
+                        var self = map.get(request.self)
+                        var keys = Reflect.ownKeys(self).filter(i => typeof i === 'string')
+                        return JSON.stringify(keys)
+                    case COMMANDS.GET_OWN_PROPERTY_DESCRIPTOR:
+                        var self = map.get(request.self)
+                        var prop = request.prop
+                        var desc = Object.getOwnPropertyDescriptor(self, prop)
+                        delete desc.value
+                        delete desc.get
+                        delete desc.set
+                        return JSON.stringify(desc)
+                    case COMMANDS.UNREF:
+                        map.delete(request.ref)
+                        return JSON.stringify({success: true})
+                }
+            
+                return '{"error":"not implement"}'
+            })
+
+            return payload
+        },
+
+        createProxy (payload) {
+            var data = payload
+            var buffer = data.buffer
+            var int32 = data.int32
+            var constants = data.constants
+            var dataView = data.dataView
+            let id = 0
+        
+            const decoder = new TextDecoder()
+            const encoder = new TextEncoder()
+            
+            function parse(buffer, offset, length) {
+                var slice = buffer.slice(offset, offset + length)
+                var text = decoder.decode(new Uint8Array(new Uint8Array(slice)))
+                return text
+            }
+        
+            function write(buffer, offset, text) {
+                var encodedBuffer = encoder.encode(text)
+                new Uint8Array(buffer).set(encodedBuffer, offset)
+        
+                return encodedBuffer.byteLength
+            }
+        
+            var send = (text) => {
+                var length = write(buffer, constants.I32_DATA_INDEX * 4, text)
+                dataView.setUint32(constants.I32_DATA_LENGTH_INDEX * 4, length)
+        
+                const old = Atomics.load(int32, constants.I32_CHILD_LOCK_INDEX)
+                Atomics.store(int32, constants.I32_PARENT_LOCK_INDEX, id++)
+                Atomics.notify(int32, constants.I32_PARENT_LOCK_INDEX)
+                Atomics.wait(int32, constants.I32_CHILD_LOCK_INDEX, old)
+        
+                var length = dataView.getUint32(constants.I32_DATA_LENGTH_INDEX * 4)
+                var text = parse(buffer, constants.I32_DATA_INDEX * 4, length)
+        
+                return text
+            }
+        
+            
+            var finalizerGroup = new FinalizationGroup(function (refId) {
+                send(JSON.stringify({ command: COMMANDS.UNREF, ref: refId }))
+            })
+
+            function createProxy(refId) {
+                var proxy = new Proxy({}, {
+                    get: function(target, prop, receiver) {
+                        var result = JSON.parse(send(JSON.stringify({ command: COMMANDS.GET_PROPERTY, self: refId, prop: prop })))
+
+                        switch (result.type) {
+                            case TYPES.NUMBER:
+                            case TYPES.BOOLEAN:
+                            case TYPES.STRING:
+                                return result.value
+                            case TYPES.NULL:
+                                return null
+                            case TYPES.UNDEFINED:
+                                return undefined
+                            case TYPES.OBJECT:
+                                return createProxy(result.ref)
+                        }
+                    },
+                    ownKeys: function(target) {
+                        var result = JSON.parse(send(JSON.stringify({ command: COMMANDS.GET_OWN_KEYS, self: refId })))
+                        return result;
+                    },
+                    getOwnPropertyDescriptor(target, prop) {
+                        var result = JSON.parse(send(JSON.stringify({ command: COMMANDS.GET_OWN_PROPERTY_DESCRIPTOR, self: refId, prop })))
+                        Reflect.defineProperty(target, prop, result);
+                        return result
+                    }
+                })
+
+                finalizerGroup.register(proxy, refId, proxy)
+
+                return proxy
+            }
+
+            function getRoot () {
+                var result = JSON.parse(send(JSON.stringify({ command: COMMANDS.GET_ROOT })))
+                return createProxy(result.ref)
+            }
+
+            return getRoot()
+        }
+    }
+}
--- worker.js
+++ worker.js
@@ -1,101 +1,14 @@
-function main (callback) {
-    self.addEventListener('message', function (event) {
-        var data = event.data
-        var buffer = data.buffer
-        var int32 = data.int32
-        var constants = data.constants
-        var dataView = data.dataView
-        let id = 0
-    
-        const decoder = new TextDecoder()
-        const encoder = new TextEncoder()
-        
-        function parse(buffer, offset, length) {
-            var slice = buffer.slice(offset, offset + length)
-            var text = decoder.decode(new Uint8Array(new Uint8Array(slice)))
-            return text
-        }
-    
-        function write(buffer, offset, text) {
-            var encodedBuffer = encoder.encode(text)
-            new Uint8Array(buffer).set(encodedBuffer, offset)
-    
-            return encodedBuffer.byteLength
-        }
-    
-        var send = (text) => {
-            var length = write(buffer, constants.I32_DATA_INDEX * 4, text)
-            dataView.setUint32(constants.I32_DATA_LENGTH_INDEX * 4, length)
-    
-            const old = Atomics.load(int32, constants.I32_CHILD_LOCK_INDEX)
-            Atomics.store(int32, constants.I32_PARENT_LOCK_INDEX, id++)
-            Atomics.notify(int32, constants.I32_PARENT_LOCK_INDEX)
-            Atomics.wait(int32, constants.I32_CHILD_LOCK_INDEX, old)
-    
-            var length = dataView.getUint32(constants.I32_DATA_LENGTH_INDEX * 4)
-            var text = parse(buffer, constants.I32_DATA_INDEX * 4, length)
-    
-            return text
-        }
-    
-        callback(send)
-    })
-}
+importScripts('dom-proxy.js')
 
-var COMMANDS = {
-    GET_ROOT: 'GET_ROOT',
-    GET_PROPERTY: 'GET_PROPERTY',
-    UNREF: 'UNREF'
-}
-
-var TYPES = {
-    NUMBER: 'NUMBER',
-    BOOLEAN: 'BOOLEAN',
-    STRING: 'STRING',
-    NULL: 'NULL',
-    UNDEFINED: 'UNDEFINED',
-    OBJECT: 'OBJECT'
-}
-
-function registerUnref() {}
-
-main(function (send) {
-    var finalizerGroup = new FinalizationGroup(function (refId) {
-        send(JSON.stringify({ command: COMMANDS.UNREF, ref: refId }))
-    })
-
-    function createProxy(refId) {
-        var proxy = new Proxy({}, {
-            get: function(target, prop, receiver) {
-                var result = JSON.parse(send(JSON.stringify({ command: COMMANDS.GET_PROPERTY, self: refId, prop: prop })))
-
-                switch (result.type) {
-                    case TYPES.NUMBER:
-                    case TYPES.BOOLEAN:
-                    case TYPES.STRING:
-                        return result.value
-                    case TYPES.NULL:
-                        return null
-                    case TYPES.UNDEFINED:
-                        return undefined
-                    case TYPES.OBJECT:
-                        return createProxy(result.ref)
-                }
-            }
-        })
-
-        finalizerGroup.register(proxy, refId, proxy)
-
-        return proxy
-    }
-
-    function getRoot () {
-        var result = JSON.parse(send(JSON.stringify({ command: COMMANDS.GET_ROOT })))
-        return createProxy(result.ref)
-    }
-
-    var remoteWindow = getRoot()
+self.addEventListener('message', function (event) {
+    var data = event.data
+    var remoteWindow = DOMProxy.createProxy(data)
 
     console.log(remoteWindow.hostData.test.test2)
     console.log(remoteWindow.location.href)
+    console.log(Object.keys(remoteWindow))
+    console.log(Object.getOwnPropertyNames(remoteWindow.location))
+    for (let key of Object.getOwnPropertyNames(remoteWindow.location)) {
+        console.log(key, remoteWindow.location[key])
+    }
 })
