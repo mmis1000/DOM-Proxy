f24218aaf8987be6eb96469a1cdeb4d87b608807
mmis1000 <mmis10002@gmail.com>
2019-06-25 02:37:10 +0000

    Update demo

--- worker.js
+++ worker.js
@@ -4,13 +4,14 @@ self.addEventListener('message', function (event) {
     var payload = event.data
     var remoteWindow = DOMProxy.createProxy(payload)
 
-    console.log(remoteWindow.hostData.test.test2)
-    console.log(remoteWindow.location.href)
-    console.log(remoteWindow.document.title)
-    console.log(remoteWindow.document === remoteWindow.document)
-    console.log(Object.keys(remoteWindow))
-    console.log(Object.getOwnPropertyNames(remoteWindow.location))
+    console.log('Host js object: ', remoteWindow.hostData.test.test2)
+    console.log('Host location: ', remoteWindow.location.href)
+    console.log('Host window title: ', remoteWindow.document.title)
+    console.log('Strict equal: ', remoteWindow.document === remoteWindow.document)
+    console.log('Host window keys: ', Object.keys(remoteWindow))
+    console.log('Host location keys: ', Object.getOwnPropertyNames(remoteWindow.location))
     for (let key of Object.getOwnPropertyNames(remoteWindow.location)) {
-        console.log(key, remoteWindow.location[key])
+        console.log('Host location property: ', key, remoteWindow.location[key])
     }
+    self.remoteWindow = remoteWindow
 })
